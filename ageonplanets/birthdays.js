61562fb6b01bd327151ce445a92600f97b845493
ml-astro <72692667+ml-astro@users.noreply.github.com>
2021-10-22 20:18:57 +0300

    big update

--- ageonplanets/birthdays.js
+++ ageonplanets/birthdays.js
@@ -1,27 +1,30 @@
 let planets = new Map([
-    ['mercury', 87.969*86400*1000],
-    ['venus', 224.701*86400*1000],
-    ['mars', 686.98*86400*1000],
-    ['jupiter', 4332.589*86400*1000],
-    ['saturn', 10759.22*86400*1000],
-    ['uranus', 30685.4*86400*1000],
-    ['neptune', 60190.03*86400*1000]
+    ['mercury', 87.969*86400],
+    ['venus', 224.701*86400],
+    ['mars', 686.98*86400],
+    ['jupiter', 4332.589*86400],
+    ['saturn', 10759.22*86400],
+    ['uranus', 30685.4*86400],
+    ['neptune', 60190.03*86400]
 ])
 
-/*const mercury = 87.969*86400*1000
-const venus = 224.701*86400*1000
-const mars = 686.98*86400*1000
-const jupiter = 4332.589*86400*1000
-const saturn = 10759.22*86400*1000
-const uranus = 30685.4*86400*1000
-const neptune = 60190.03*86400*1000*/
+function ageFormat (age){
+    if(age%10 == 2 || age%10 == 3 || age%10 == 3){
+        return age+' года'
+    }
+    else if(age%10 == 1){
+        return age+' год'
+    }
+    return age+' лет'
+}
 
 function currentAge (){
     let birthday = new Date(document.getElementById('birthdate').value)
     let today = new Date()
     let range = today - birthday
     planets.forEach((period, planet) => {
-        document.getElementById(planet+'Result').value=Math.floor(range/period)
+        document.getElementById(planet+'Age').innerHTML=ageFormat(Math.floor(range/(period*1000)))
+        //document.getElementById(planet+'Age').innerHTML=Math.floor(range/(period*1000))
     })
 }
 
@@ -31,9 +34,9 @@ function nextDate (){
     let nextBday = new Date (birthday)
     planets.forEach((period,planet)=>{
         while (today > nextBday){
-            nextBday =  new Date (nextBday.setMilliseconds(nextBday.getMilliseconds()+period))
+            nextBday =  new Date (nextBday.setSeconds(nextBday.getSeconds()+period))
         }
-        document.getElementById(planet+'BD').value=nextBday.getDate()+'.'+(nextBday.getMonth()+1)+'.'+nextBday.getFullYear()
+        document.getElementById(planet+'BD').innerHTML=nextBday.getDate()+'.'+(nextBday.getMonth()+1)+'.'+nextBday.getFullYear()
         nextBday = new Date (birthday)
     })
     
