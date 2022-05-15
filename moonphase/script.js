31ac524024814c855806f177b346e7f93e3d85ef
Maksim Ljovin <maksim.ljovin@outlook.com>
2022-05-15 10:20:57 +0300

    now with month forecast

--- moonphase/script.js
+++ moonphase/script.js
@@ -1,96 +1,191 @@
 ///algorithm for the moon longitude from https://www.aa.quae.nl/en/reken/hemelpositie.html#4
 
-let day0 = new Date(Date.UTC(2000,0,1,12,0,0)).getTime();
-let today = new Date().getTime();
-let now = new Date();
+//display current date and time
+const now = new Date();
 const paragraph = document.querySelector('.date');
 paragraph.innerHTML+=`${now.toLocaleDateString()} ${now.getHours()}:${now.getMinutes()<10?('0'+now.getMinutes()):now.getMinutes()}`;
+///////////////
 
-let interval = (today - day0)/86400000;
-let L = (218.316 + 13.176396*(interval))%360;
-let M = (134.963 + 13.064993*(interval))%360;
-let moonLongitude = L + 6.289 * Math.sin(M*0.0174533);
-let equinox = new Date(Date.UTC(2022,2,20,15,33,0)).getTime();
-let sunLongitude = ((today - equinox)/86400000)/365.256*360;
-let angle = moonLongitude - sunLongitude;
-if(angle < 0){
-    //not sure if this is right
-    angle += 360;
+//variables for the current moonphase calculation
+const day0 = new Date(Date.UTC(2000,0,1,12,0,0)).getTime();
+//const today = new Date(Date.UTC(2022,4,14,12,0,0)).getTime();
+const today = new Date().getTime();
+const interval = (today - day0)/86400000;
+let angle = getMoonAngle(interval,today);
+////////////
+//calculates longitude for the provided day
+//returns angle difference between sun and moon (phase)
+function getMoonAngle(days,dayNow){
+    let L = (218.316 + 13.176396*(days))%360;
+    let M = (134.963 + 13.064993*(days))%360;
+    let moonLongitude = L + 6.289 * Math.sin(M*0.0174533);
+    let equinox = new Date(Date.UTC(2022,2,20,15,33,0)).getTime();
+    let sunLongitude = ((dayNow - equinox)/86400000)/365.256*360;
+    let angle = moonLongitude - sunLongitude;
+    if(angle < 0){
+        //not sure if this is right
+        angle += 360;
+    }
+    if(angle > 360){
+        angle = angle % 360;
+    }
+    return angle;
 }
-if(angle > 360){
-    angle = angle % 360;
+
+//how close is the calculated phase to a main phase
+function proximityToPhase(a){
+    let remain = a%90;
+    if(Math.abs(remain-90)<remain){
+        return(Math.abs(remain-90));
+    }
+        return remain
+}
+
+//converts interval to date
+function intervalToDate(days){
+    let date = new Date(day0);
+    date.setDate(date.getDate()+days);
+    return date.toDateString();
+}
+
+function addLeadZero(number){
+    if(number < 10){
+        return number = '0'+number;
+    }
+    return number;
+}
+
+//do the month forecast for moon phases
+function makeForecast(){
+    let dateNow = new Date (intervalToDate(Math.floor(interval))).getTime();
+    let forecast = []
+    let newInterval = Math.floor(interval);
+    let angularDifference = 90;
+    let currentAngle, newBestDay, oldComparisonAngle, newComparisonAngle,proximity;
+    
+    while(forecast.length < 4){
+        newInterval++;
+        dateNow+=86400000;
+        currentAngle = getMoonAngle(newInterval,(dateNow));
+        //comparing to an angle a bit smaller
+        //if the angle is less than 90, add 360 to do right comparison with the previous date which was almost 360
+        if(currentAngle<20){
+            currentAngle+=360
+        }
+
+        newComparisonAngle = getComparisonAngle(currentAngle-15);
+        proximity = proximityToPhase(currentAngle);
+
+        if(proximity < angularDifference) {
+            newBestDay = newInterval;
+            angularDifference = Math.abs(newComparisonAngle - currentAngle);
+            bestAngle = currentAngle;
+            oldComparisonAngle = newComparisonAngle;
+        }
+        if(proximity > angularDifference){
+            newComparisonAngle = getComparisonAngle(currentAngle-15);
+            angularDifference = 90;
+            forecast.push([intervalToDate(newBestDay),oldComparisonAngle])
+        }
+        let date = new Date(day0);
+        date.setDate(date.getDate()+newInterval);
+    }
+    return forecast;
+}
+let forecast = makeForecast();
+
+//nearest main phase
+//receives moon-sun angle
+//returns main phase angle
+function getComparisonAngle(angle){
+    switch (true) {
+        case angle < 90:
+            comparison = 90
+            break
+        case angle < 180:
+            comparison = 180
+            break
+        case angle < 270:
+            comparison = 270
+            break
+        case angle < 360:
+            comparison = 360
+    }
+    return comparison;
 }
 
 //display phase name
-switch (true) {
-    case angle < 10 || angle >= 350:
-        phaseName = 'Новолуние';
-        break;
-    case angle < 80:
-        phaseName = 'Молодая Луна';
-        break;
-    case angle < 100:
-        phaseName = 'Первая четверть';
-        break;
-    case angle < 170:
-        phaseName = 'Растущая Луна';
-        break;
-    case angle < 190:
-        phaseName = 'Полнолуние';
-        break;
-    case angle < 260:
-        phaseName = 'Убывающая Луна';
-        break;
-    case angle < 280:
-        phaseName = 'Последняя чеверть';
-        break;
-    case angle < 350:
-        phaseName = 'Старая Луна';
-        break;
+function getPhaseName(phaseAngle){
+    let moonName;
+    switch (true) {
+        case phaseAngle < 10 || phaseAngle >= 350:
+            moonName = 'Новолуние';
+            break;
+        case phaseAngle < 80:
+            moonName = 'Молодая Луна';
+            break;
+        case phaseAngle < 100:
+            moonName = 'Первая четверть';
+            break;
+        case phaseAngle < 170:
+            moonName = 'Растущая Луна';
+            break;
+        case phaseAngle < 190:
+            moonName = 'Полнолуние';
+            break;
+        case phaseAngle < 260:
+            moonName = 'Убывающая Луна';
+            break;
+        case phaseAngle < 280:
+            moonName = 'Последняя чеверть';
+            break;
+        case phaseAngle < 350:
+            moonName = 'Старая Луна';
+            break;
+    }
+    return moonName;
 }
 
-//calculating the phase
+//calculating the phase %
 switch(true){
     case angle < 90:
         //(R-b)/2R
         //0.0174533 = 1 degree in radians
-        phase = Math.round(((1-Math.cos(angle*0.0174533))/2)*100);
+        phase = Math.round(((1-Math.cos(angle*0.017453292))/2)*100);
         break;
     case angle < 180:
         //180-A
         //(R+b)/2R
-        phase = Math.round(((1+Math.cos((180-angle)*0.0174533))/2)*100);
+        phase = Math.round(((1+Math.cos((180-angle)*0.017453292))/2)*100);
         break;
     case angle < 270:
         //A-180
         //(R+b)/2R
-        phase = Math.round(((1+Math.cos((angle-180)*0.0174533))/2)*100);
+        phase = Math.round(((1+Math.cos((angle-180)*0.017453292))/2)*100);
         break;
     case angle < 360:
         //360-A
         //(R-b)/2R
-        phase = Math.round(((1-Math.cos((360-angle)*0.0174533))/2)*100);
+        phase = Math.round(((1-Math.cos((360-angle)*0.017453292))/2)*100);
         break;
 }
 
-//display the info
+//displays the description
 document.querySelector('.description').innerHTML=`
 Луна освещена на ${phase}%`;
-document.querySelector('.phase').innerHTML = `<b>${phaseName}</b>`;
+document.querySelector('.phase').innerHTML = `<b>${getPhaseName(angle)}</b>`;
 
-/*
-//will implement sometime later
-var canvas = document.getElementById("canvas"),
-    ctx = canvas.getContext("2d");
-var background = new Image();
-background.src = "./moon.png";
-// Make sure the image is loaded first otherwise nothing will draw.
-background.onload = function(){
-    ctx.drawImage(background,0,0);
-    // Draw whatever else over top of it on the canvas.
-}*/
+document.querySelector('.forecast').innerHTML+='<ul>'
+for(i=0; i<forecast.length; i++){
+    let date = new Date(forecast[i][0]).getDate();
+    let month = new Date(forecast[i][0]).getMonth()+1;
+    let year = new Date(forecast[i][0]).getFullYear();
+    document.querySelector('.forecast').innerHTML+=`<li><img src='${forecast[i][1]}.png'>${addLeadZero(date)}.${addLeadZero(month)}.${year}</li>`
+}
+document.querySelector('.forecast').innerHTML+='</ul>'
 
-//draw moons
+
+//draws moon phase
 switch (true){
     case angle < 90:
         drawMoon('#EEE','#2a303a','#2a303a',100-phase*2,100,-1);
